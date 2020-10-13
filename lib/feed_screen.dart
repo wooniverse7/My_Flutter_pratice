5f72bdf93f026112c114235901c6ed38c9681d98
wooniverse7 <wooniverse7@gmail.com>
2020-10-13 14:05:51 +0900

    backup01

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -3,17 +3,21 @@ import 'package:flutter/material.dart';
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(//StatefullWidget의 기능 사용허가
-     appBar: AppBar(
-       title: Text(
-         'Sharing a Walk Course',
-         style: TextStyle(
-           fontFamily: 'veganStyle',
-         color: Colors.black87,
-
-         ),
-       ),
-     ),
+    return Scaffold( //StatefullWidget의 기능 사용허가
+      appBar: AppBar(
+        title: Text(
+          'Sharing a Walk Course',
+          style: TextStyle(fontFamily: 'veganStyle', color: Colors.black87,),
+        ),
+      ),
+      body: ListView.builder(itemBuilder: feedListBuilder, itemCount: 30,),
+    );
+  }
+    Widget feedListBuilder(BuildContext context, int index){
+    return Container(
+      color: Colors.accents[index % Colors.accents.length],
+      height: 100,
     );
   }
 }
+
